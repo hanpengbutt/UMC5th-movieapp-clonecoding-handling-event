2bca06d1b50d90ebc46940f07e12c73ca35afc54
한지은 <hjn510450@gmail.com>
2023-11-07 22:31:32 +0900

    헤더 생성 및 라우팅 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import MovieContainer from "./components/movieList/MovieContainer.style";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import MovieContainer from "./components/movie/MovieContainer";
+import Home from "./components/home/Home";
+import "./App.css";
+import Celebirity from "./components/celebirity/Celebirity";
+import Tv from "./components/tv/Tv";
 
 function App() {
   return (
     <div className="App">
-      <MovieContainer />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie" element={<MovieContainer />} />
+          <Route path="/tv" element={<Tv />} />
+          <Route path="/celebirity" element={<Celebirity />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
